f77dc3db2e3acdd80ab7efdc4f1018056faf735b
wanxiankai <xiankai.wan@gmail.com>
2025-06-20 06:11:49 +0800

    feat: add assets index

---
+++ src/assets/index.js
@@ -0,0 +1,64 @@
+import logo from "./logo.png";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import magnetlabs from "./company/magnetlabs.png";
+import xmly from "./company/xmly.png";
+import boohee from "./company/boohee.png";
+import alibaba from "./company/alibaba.jpeg";
+import italki from "./company/italki.jpeg";
+
+import talk2ai from "./talk2ai.png";
+import crypt from "./crypt.png";
+import portfolio from "./portfolio.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  magnetlabs,
+  xmly,
+  boohee,
+  alibaba,
+  italki,
+  talk2ai,
+  crypt,
+  portfolio
+};
