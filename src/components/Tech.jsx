6df814245540625c54403eb94f62aa05b929d5f8
wanxiankai <xiankai.wan@gmail.com>
2025-06-20 13:42:29 +0800

    feat: add lazyload for techs icon

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -3,13 +3,16 @@ import React from "react";
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
+import LazyLoad from "./LazyLoad";
 
 const Tech = () => {
   return (
     <div className='flex flex-row flex-wrap justify-center gap-10'>
       {technologies.map((technology) => (
         <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
+          <LazyLoad>
+            <BallCanvas icon={technology.icon} />
+          </LazyLoad>
         </div>
       ))}
     </div>
