140872ecf619e7412ed052f787a90aa280017d16
wanxiankai <xiankai.wan@gmail.com>
2025-06-20 16:45:23 +0800

    feat: complete the emailjs

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,8 +33,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        import.meta.env.VITE_EMAILJS_SERVICEID,
+        import.meta.env.VITE_EMAILJS_TEMPLATEID,
         {
           from_name: form.name,
           to_name: "Xiankai Wan",
@@ -42,7 +42,7 @@ const Contact = () => {
           to_email: "xiankai.wan@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
       )
       .then(
         () => {
